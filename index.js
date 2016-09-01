ac189915d1f903fb8b3621000c05dc64994a488c
Jason Swinehart <jasonswinehartwv@gmail.com>
2016-09-01 01:26:08 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,3 @@
+console.log("Thank you for using JS Bank.  Using the buttons, please provide us with your PIN to access your account.")
+console.error("Do not use this account!  It is not safe!")
+console.warn("You will leave yourself with less that the amount allowed.")
