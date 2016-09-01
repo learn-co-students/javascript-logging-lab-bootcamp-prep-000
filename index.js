82e723e81817b9d2f64d79094fa9a8f0fc0b33ca
Dharma theja Navuluri <dharma786theja@aol.com>
2016-09-01 05:23:38 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,5 @@
+console.error("Something went wrong! Please check")
+
+console.log("The function X is being executed. Please wait to finish")
+
+console.warn("The function is not up to the standard. Please update as soon as possible")
