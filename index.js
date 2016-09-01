59b916b0397745f1c9465bf147a8a8bcf0ea24cd
jammyfish <jkhanfri@gmail.com>
2016-09-01 02:41:56 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,3 @@
+console.error("well golly, this is frustrating!")
+console.log("merp, I am supposed to be doing work but wait, this is my work, herher -cries-")
+console.warn('Houston we have a problem')
