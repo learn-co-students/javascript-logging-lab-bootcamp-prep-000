0d9143d4c0529ed0ed3fdd5da8ba396d67835a3a
Gabriel Abreu <gabreu554@gmail.com>
2016-09-01 12:16:10 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,3 @@
+console.log("Please wait while the money is being prepared.")
+console.warn("Users will be locked out after 3 incorrect attempts")
+console.error("ATM needs a reboot. We apologize for the inconvenience")
