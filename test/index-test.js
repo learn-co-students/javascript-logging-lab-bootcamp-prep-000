6f0d81c4d7a7729f65caee9d1868d87810d8ad57
Vikram B <vikramdeep.b@gmail.com>
2016-09-05 18:42:30 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -7,6 +7,7 @@ const path = require('path')
 describe('index', () => {
   const html = '<div></div>'
   const src = path.resolve(__dirname, '..', 'index.js')
+  console.error("HALP!")
 
   it('calls console.error()', done => {
     const spy = expect.spyOn(console, 'error').andCallThrough()
