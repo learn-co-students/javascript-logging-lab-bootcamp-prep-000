467536f78f3b076f12bc089de04cea621bab3d33
Tajinder Bahia <whoopbaepsae@gmail.ocm>
2016-09-09 20:58:33 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,5 @@
+console.error("Jackson Wang is ultra bae");
+
+console.log("No one can argue with the fact that Jackson's flips are magical!");
+
+console.warn("Argue with me and the army of Jackson stans will come for you.");
