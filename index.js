9a61ebdd39f31b2bdd751cf0aef07d3c18f3d7ef
Farshid Zaman <farshid_bd@hotmail.com>
2016-09-11 19:51:30 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,3 @@
+console.error("This machine can not accept bills right now. SOrry! :( ")
+console.warn("Do not put more than 20 bills together")
+console.log("A transaction was made by Mr. Farshid Zaman")
