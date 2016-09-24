055f2557ca56f5a253a12262ed8f2ba084bc6be3
Ashley Robinson <ashleyhowerobinson@gmail.com>
2016-09-24 20:11:27 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,3 @@
+console.error("Oh my! The house is burning down because the microwave mutinied!")
+console.log("The icemaker rallied and put out the flames")
+console.warn("You might want to check and see about the microwave's feelings")
