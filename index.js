13dbd9c5661b8415bbb2c18c9913efa2cfb45c90
Leaticia Osuagwu <leaticiaasuzu@yahoo.com>
2016-09-25 00:10:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 console.error("HALP!")
 console.warn("the ATM is broken! Do not use!")
 console.log("i will not use ATM again. It does not work")
+input.name("enter your name")
+//declare the variable
+var word
+word='bird'
+
+console.log(word)
+word='dog'
+
+consle.log(word)
+var word='bird'
+window.word
+var greeting="Hello, everyone!"
+
+var specialGuest="Neil deGegrasse Tyson"
+"hello, "+specialGuest+"!"
+var topic="space"
+
+var conversation='${topic}'
