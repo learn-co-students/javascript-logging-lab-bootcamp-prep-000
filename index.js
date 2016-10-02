639c961c5e518bba7c96bc007ea5bd3fa9c5ed8f
Kyle Lee <hello.kylelee@gmail.com>
2016-10-02 04:26:40 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,3 @@
+console.error("Hello there!? How can you say that when this thing just BROKE!")
+console.log("Oh, well hello there. I'm calm.")
+console.warn("It hasn't broken yet. It's just a warning. Chill out.")
