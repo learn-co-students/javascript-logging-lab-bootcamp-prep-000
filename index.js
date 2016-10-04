f467c84e3fb826b20a297037facae322d27cca4e
Leonna Hill <leonnahill@gmail.com>
2016-10-04 02:52:17 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,3 @@
+console.error("Chocolate has been removed from the environment");
+console.log("It seems like Hyland has no longer been allowed to eat sugar.");
+console.warn("Sugar has been introduced into Hyland\'s environment!");
