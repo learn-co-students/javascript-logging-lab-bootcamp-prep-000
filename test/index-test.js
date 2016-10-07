bea1974f0213940b778579fa823f8929be19172a
Nicolas Rodriguez Gregorio <nicolas.rodriguezg.ssh@gmail.com>
2016-10-07 19:10:16 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -3,18 +3,17 @@ const fs = require('fs')
 const jsdom = require('jsdom')
 const path = require('path')
 
-
 describe('index', () => {
   const html = '<div></div>'
   const src = path.resolve(__dirname, '..', 'index.js')
 
+
   it('calls console.error()', done => {
     const spy = expect.spyOn(console, 'error').andCallThrough()
-
     jsdom.env(html, [src], {
       virtualConsole: jsdom.createVirtualConsole().sendTo(console)
     }, (err, window) => {
-      expect(spy).toHaveBeenCalled('expected console.error to have been called')
+      expect(spy).toHaveBeenCalled('console.error("HALP!");')
       console.error.restore()
       done()
     })
