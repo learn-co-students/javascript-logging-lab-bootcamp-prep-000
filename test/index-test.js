5609a05cc82f55e4c2505152d6a854ab54caf62f
Felix Dumas <felix_dumas@yahoo.com>
2016-10-15 15:38:19 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -8,7 +8,7 @@ describe('index', () => {
   const html = '<div></div>'
   const src = path.resolve(__dirname, '..', 'index.js')
 
-  it('calls console.error("HALP!")', done => {
+  it('calls console.error()', done => {
     const spy = expect.spyOn(console, 'error').andCallThrough()
 
     jsdom.env(html, [src], {
