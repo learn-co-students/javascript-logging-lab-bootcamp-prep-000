35fffdce1efc9db01a4e85e1498ba9b25c2c815e
Varun T <varun.kp63@gmail.com>
2016-10-17 01:28:05 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,3 @@
+console.log('I will be logging out soon, save all your files.');
+console.warn('This is Just a warning, next if you repeat this step it will be shut down.');
+console.error('Test Error Scenario - Shutting down.');
