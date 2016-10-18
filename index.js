d9313fc9c12bb05261bd4977b4b5815822cb19ee
Wadson Fleurigene <wadsonf@gmail.com>
2016-10-18 03:08:20 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,3 @@
+console.error("This is probably going to be the longest error message you might ever experience.")
+console.log("We are logging a random error here.")
+console.warn("This is a warning. I am on a crazy combo streak and I love the animations.")
