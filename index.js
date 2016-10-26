1bf839fb767bb43512522f659c50dc676dce28ad
A Noel <aliciaknoel@gmail.com>
2016-10-26 20:11:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 console.error("Halp!")
-console.warn(Beware!)
 console.warn("Beware")
+console.log("I would like to be a logger")
+console.log("I would be a logger.")
+console.log("I would be a logger.")
