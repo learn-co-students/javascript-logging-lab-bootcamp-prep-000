a6298c71672489c8198645144745595ef51be494
Mariel Arismendy <isagts@gmail.com>
2016-11-08 22:16:11 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -10,6 +10,7 @@ describe('index', () => {
 
   it('calls console.error()', done => {
     const spy = expect.spyOn(console, 'error').andCallThrough()
+    console.error("HALP")
 
     jsdom.env(html, [src], {
       virtualConsole: jsdom.createVirtualConsole().sendTo(console)
@@ -22,6 +23,7 @@ describe('index', () => {
 
   it('calls console.log()', done => {
     const spy = expect.spyOn(console, 'log').andCallThrough()
+    console.log("I'll be the Logger IsaGTS")
 
     jsdom.env(html, [src], {
       virtualConsole: jsdom.createVirtualConsole().sendTo(console)
@@ -34,6 +36,7 @@ describe('index', () => {
 
   it('calls console.warn()', done => {
     const spy = expect.spyOn(console, 'warn').andCallThrough()
+    console.warn("Careful with the Spies")
 
     jsdom.env(html, [src], {
       virtualConsole: jsdom.createVirtualConsole().sendTo(console)
