2620293b28417501d0c10429890e4a1ee416ffa4
Tonya  Ross <trossgb@gmail.com>
2016-11-18 05:40:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
 console.error("HALP")
-Learn Tests
-console.error("HALP")
+console.log('log');
+console.warn('warn');
 learn test
-console.error("HALP2")
-learn test2
-console.log("HALP")
-learn test
-console.log("I would be a logger")
-learn submit
-console.warn("URGENT");
-learn submit
