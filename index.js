69844e447dbc09f9fd68cb08ace222e266a474d1
Caroline Kasterine <ckasterine@gmail.com>
2016-11-24 01:17:38 +0000

    Done.

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ console.console.error("HALP!");
 console.console.log("I am trying this");
 console.console.log("try anything");
 console.console.warn("who knows");
+console.console.log('call again');
+console.console.warn('try again');
