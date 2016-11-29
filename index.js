c3d8b265dd62afc7ca59b58826e14ecf02828ac8
Christian Evans <evanschristian93@gmail.com>
2016-11-29 22:02:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-const expect = require('expect')
-const fs = require('fs')
-const jsdom = require('jsdom')
-const path = require('path')
-
-describe('index', () => {
-  // there's stuff in here, too
-})
-
-it('calls console.error()', () => {
-  // this is where the tests are!
-})
-
-jsdom({
-  src: fs.readFileSync(path.resolve(__dirname, '..', 'index.js'), 'utf-8')
-})
-
 console.error("HALP!")
 console.log("I would be a logger.")
 console.warn("Might not want to do that")
