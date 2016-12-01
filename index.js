61f09d69a6ba74f0b5bf3960b9af4ad8b8ceb66c
Brady Zimmer <bradyzimmer@outlook.com>
2016-12-01 04:01:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-console.error("Halp!");
-console.log ("$25 will be taken out of acct 2556")
-console.warn("If you proceed you will overdraft your acct.");
+index calls console.error("Halp!");
+index calls console.log ("$25 will be taken out of acct 2556")
+index calls console.warn("If you proceed you will overdraft your acct.");
