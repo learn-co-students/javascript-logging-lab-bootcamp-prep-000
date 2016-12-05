0695cb545e6002e75687d7fd90e3b08a1905ec32
Rubens Jean-Paul <rubens.jeanpaul@gmail.com>
2016-12-05 22:03:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 console.error('HALP!');
-  console.warn();
-  console.log();
+console.warn('Wrong Pin B');
+console.log('You got a big amount, this is referencing the 2 Chaniz song that talks about having large sums of cash in ones hand or bank account');
