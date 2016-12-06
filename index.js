8792bdce2331020acd61448df31c38b28ba81887
Austin Truscott <austin.truscott@hotmail.com>
2016-12-06 00:11:30 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,7 @@
+console.error("HALP")
+console.log("Fix me")
+console.log("HALP")
+console.log("I make music.")
+console.log("I would be a logger.") // get it?
+console.warn("Leave the cursor on the same line") // During tests dont fuck up
+console.warn("Why don't you work") //WHY?!
