8293ac3c04f0081273ef798c59e0327a0f97bfef
Cherith Simmons <cherithsimmons@gmail.com>
2016-12-09 19:08:33 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,3 @@
+console.error ("Bitch Better Have My Money")
+console.log ("Love On The Brain")
+console.warn ("Anti")
--- test/index-test.js
+++ test/index-test.js
@@ -40,7 +40,7 @@ describe('index', () => {
     }, (err, window) => {
       expect(spy).toHaveBeenCalled('expected console.warn to have been called')
       console.warn.restore()
-      done()
+      done()f
     })
   })
 })
