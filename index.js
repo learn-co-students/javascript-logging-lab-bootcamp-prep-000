260d092b35f43b392f80b856712f863f85851873
George Buckley <g.buckley@live.com>
2016-12-13 17:37:12 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,3 @@
+console.error("STOP! index.js called an error!");
+console.log("Dear log, Let's call a console warning.");
+console.warn("Just a friendly warning, you might be doing something wrong!");
