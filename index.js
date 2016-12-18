37205f21f5507cda5f51bfd55922fea7acf949f7
Chris Dziewa <chris.dziewa@gmail.com>
2016-12-18 00:32:23 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,5 @@
+console.error("The program has encountered an error");
+
+console.log("They call me a lumberjack for all the logging I've done. No trees were ever harmed though");
+
+console.warn("WARNING: This program is currently boring");
