cba8a61ad46e63d5e4fef98e3eb5cc6510cd5bc7
Reymaldo Rodriguez <rrodr186@fiu.edu>
2016-12-23 08:11:10 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,3 @@
+console.error("some error!")
+console.log("I'm learning about console.log, console.error and console.earn functions. To call them I can use ().")
+console.warn("I should be careful when I use strings and numbers with javascript.")
