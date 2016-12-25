0ebb46e3840efaee581ec555e92d57b8943d29b7
Raj Patel <raj7@yahoo.com>
2016-12-25 21:56:14 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -20,7 +20,7 @@ describe('index', () => {
     })
   })
 
-  it('calls console.log()', done => {
+  it('calls console.log("I would be a logger")', done => {
     const spy = expect.spyOn(console, 'log').andCallThrough()
 
     jsdom.env(html, [src], {
