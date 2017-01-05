5fd8f9c74bca01547458d9fc7699cd72c85b9774
Naim Muhammad <hmd139@gmail.com>
2017-01-05 15:59:39 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,8 @@
+console.error("HALP!")
+console.log("Please save this file")
+console.log("I'm trying to finish this.")
+console.log("Please complete the task.")
+console.log("I would be a developer.")
+console.error("Back away from the microwave!")
+console.error("You've made a mistake.")
+console.warn("Please don't write another line here.")
