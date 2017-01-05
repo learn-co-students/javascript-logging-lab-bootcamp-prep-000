9ffb2091ee4cb2959d74e8d8a0bf183002584f42
Cedric Kenfack <cedrickenfack14@gmail.com>
2017-01-05 06:15:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 console.error("HALP!")
-console.log("This is my first attempt at logging")
-console.warn("You might not want to use that")
+console.log("This is another try at logging")
+console.warn("You may want to stop using that")
