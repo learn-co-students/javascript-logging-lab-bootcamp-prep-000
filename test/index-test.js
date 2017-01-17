1b20688b1189fdc9feed4efbff3e97012d8915d7
Charisa Lackrone <charisaanne@gmail.com>
2017-01-17 19:08:34 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -20,7 +20,7 @@ describe('index', () => {
     })
   })
 
-  it('calls console.log()', done => {
+  it('calls console.log("I am logging.")', done => {
     const spy = expect.spyOn(console, 'log').andCallThrough()
 
     jsdom.env(html, [src], {
@@ -32,7 +32,7 @@ describe('index', () => {
     })
   })
 
-  it('calls console.warn()', done => {
+  it('calls console.warn("Warning! Warning!")', done => {
     const spy = expect.spyOn(console, 'warn').andCallThrough()
 
     jsdom.env(html, [src], {
