29ab7869b871fda272cae3c6049a66a222391822
Al Sadik Murtadha <sadiqnoori_2005@yahoo.com>
2017-01-19 00:32:18 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,7 @@
+console.error("Help!")
+console.log("Halp!")
+console.warn("Fanilly");
+console.error("It's damiged!")
+console.error("It's damiged!")
+console.log("Would yiou like to save it on the browser?")
+console.warn("You have three attempts to log in!")
