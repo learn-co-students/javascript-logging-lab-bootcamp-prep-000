0dfff54ebb963b888c1912307119c82e928136ed
Karen Yee <td2521@verizon.net>
2017-01-29 04:51:04 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,3 @@
+console.error("HALP!")
+console.log("Errors") // Print information about the program as it runs
+console.warn("This action is not wise!") // warn developers their actions might not be wise
