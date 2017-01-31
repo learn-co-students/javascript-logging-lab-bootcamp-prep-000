fb39ba54c36ac38658893514740b3657cd9afc60
Mike S <mes9180.bills@gmail.com>
2017-01-31 18:37:57 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,3 @@
+console.error('HELP! There is an error preventing me from advancing.')
+console.log("An Error occured. Please debug")
+console.warn("Please do not ignore any errors. They may be fatal")
