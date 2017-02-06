edd950d1efe42826f22b5f07035b36432aa73dcc
Asfand Chaudhry <achaudhry92@gmail.com>
2017-02-06 22:01:09 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,3 @@
+console.error("I have to copy paste the format because the parentheses on my keyboard don't work")
+console.warn("A bunch of other key son my keyboard don't work either.")
+console.log("So it takes me a bit  longer than it normally would. To complete this.")
