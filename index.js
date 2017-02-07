d0982fb4144ac1fe502f9d488a3cc68002e8c1dc
London Gallego <londonjgallego@gmail.com>
2017-02-07 19:57:45 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,3 @@
+console.error('Fix error call now please')
+console.log("now let's fix the logging error")
+console.warn("the ATM developer bit in the middle of the lesson is a bit confusing...you don't need it to fix the failed test")
