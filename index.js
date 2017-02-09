d91df000f4af69133e00ef10dc974bb04b324050
Gabe Anderson <gabrielryananderson@gmail.com>
2017-02-09 17:01:25 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,8 @@
+console.error("PIN was entered incorrectly")
+// Something broke and we are fixing momentarily.
+
+console.log("We will be updating our ATMs at 1am tonight, refrain from use.")
+// Updates
+
+console.warn("The new ATM password you created should not be used, it is too simple")
+// Letting user know to not continue with that entry.
