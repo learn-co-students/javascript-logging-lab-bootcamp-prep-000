2e1c2b159ba3a6a0026c9f6032f09129da1b8c12
Juan Diaz <juandiaz9114@gmail.com>
2017-02-13 22:19:08 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,4 @@
+console.error("Juan is learning javascript!");
+console.log("hello this is just a log");
+console.warn("I'm telling the user that this is a warning and to not continue \
+what they are doing");
