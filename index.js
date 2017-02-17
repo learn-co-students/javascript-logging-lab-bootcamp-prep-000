3bb2e612e05493ac7d92be830406293357725f97
Shurlan Mitchell <smitchell875@gmail.com>
2017-02-17 16:53:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-console.warn("wrong pin!")
-console.error("wrong password!")
-console.log("log userlogin")
-console.warn("too much")
-console.error(" keep back!")
+console.warn("keep back!")
+console.error("goodie")
+console.log("username")
