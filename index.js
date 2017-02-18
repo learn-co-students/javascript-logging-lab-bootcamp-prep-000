ebb990f571517b17f5d87ed0de536dc38eb8e10b
Sasha Ramsey <sashadotcom@me.com>
2017-02-18 14:22:43 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,4 @@
+console.error("HELP")
+console.error ("HALP!")
+console.log("ADD SOMETHING HERE!")
+console.warn("DON'T DO THAT")
--- test/index-test.js
+++ test/index-test.js
@@ -1,3 +1,5 @@
+console.error("HELP!")
+
 const expect = require('expect')
 const fs = require('fs')
 const jsdom = require('jsdom')
