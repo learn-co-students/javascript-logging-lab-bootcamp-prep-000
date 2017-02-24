aff9e900091fef4447b29dd690d7578caa1ab8e0
Victoria Ediete <scicey@gmail.com>
2017-02-24 01:54:30 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,9 @@
+console.error()
+console.error("HALP!")
+console.warn("HALP")
+console.log("HALP!")
+console.log("I would be a logger!") //get it?
+console.log("I would be a blogger!")
+console.warn("I'm loving this") //get it?
+console.warn('I\'m loving this')
+console.warn("It is well")
