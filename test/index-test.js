600b7df4fabd9667d3a8220714220f6dfe093e88
Dolores  Mutuku <androgymo@gmail.com>
2017-02-26 19:45:05 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -14,7 +14,7 @@ describe('index', () => {
     jsdom.env(html, [src], {
       virtualConsole: jsdom.createVirtualConsole().sendTo(console)
     }, (err, window) => {
-      expect(spy).toHaveBeenCalled('expected console.error to have been called')
+      expect(spy).toHaveBeenCalled('expected console.error() to have been called')
       console.error.restore()
       done()
     })
