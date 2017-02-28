3645e4f5c65eb662b3003a2a1d87549623049054
Fariha Sohail <phariha@gmail.com>
2017-02-28 18:02:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 console.error("HALP!")
 console.warn("the user should not continue doing what they just did")
 console.log("Something broke,We need to recover it!!!")
+console.updates("this program is too good!")
