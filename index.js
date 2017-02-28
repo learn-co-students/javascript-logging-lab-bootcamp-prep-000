81ae784ef917995e1e51a3d75fa7580413fd5b4f
ibrahim Sesay <ibrahimtejansesay@gmail.com>
2017-02-28 20:39:37 +0000

    Done.

--- index.js
+++ index.js
@@ -30,3 +30,11 @@ console.log("HALP!");
 console.warn():
 
 console.log("I would be a logger.") // get it?"
+
+
+console.warn("I would be a logger.");
+
+console.error("HALP!")
+console.log("I would be a logger.")
+
+console.warn()
