66ad5168d973cbe2217a10fb961ac8f920cc983a
ibrahim Sesay <ibrahimtejansesay@gmail.com>
2017-02-28 20:54:00 +0000

    Done.

--- index.js
+++ index.js
@@ -2,40 +2,4 @@ jsdom({
   src: fs.readFileSync(path.resolve(__dirname, '..', 'index.js'), 'utf-8')
 })
 
-console.console.error("HALP!");
-
-console.log("I would be a logger.")// get it?
-console.warn()
-
-
-console.log("I would be a logger.");
-console.log("I would be a logger.");
-
-console.warn("I would be a logger.");
-console.log("I would be a logger.")
-
-
-
-console.error("HALP!")
-
-console.log("I would be a logger.");
-
-console.warn("I would be a logger.");
-
-console.log("I would be a logger.");
-
-
-
-console.error("HALP!");
-console.log("HALP!");
-console.warn():
-
-console.log("I would be a logger.") // get it?"
-
-
-console.warn("I would be a logger.");
-
-console.error("HALP!")
-console.log("I would be a logger.")
-
-console.warn()
+console.error("HALP")
