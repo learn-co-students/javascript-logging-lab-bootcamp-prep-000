4bb691439e970413ce741b9f10c267bebac76668
Tracy Copeland <ayerik@hotmail.com>
2017-03-01 14:54:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-console.error("Fix!")
-console.log("Fix!")
-console.warn("Fix!")
+console.error("Sorry, the ammount you are trying to withdraw, ${withdraw} exceeds your current balance of ${balance}.")
+console.log("Please collect your cash from the dispenser.")
+console.warn("Are you sure you want to withdraw ${withdraw}?  That will leave your balance below the daily minimum balance of ${daily_minimum}.")
