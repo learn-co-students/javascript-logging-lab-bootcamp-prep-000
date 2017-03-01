46bc9b87a56c488ed0e19c0c267b3584b27e8c0a
Kinah B <learn2real@mail.com>
2017-03-01 20:18:49 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -18,6 +18,7 @@ describe('index', () => {
       console.error.restore()
       done()
     })
+    console.error('help us!')
   })
 
   it('calls console.log()', done => {
@@ -30,6 +31,7 @@ describe('index', () => {
       console.log.restore()
       done()
     })
+    console.log('getting closer')
   })
 
   it('calls console.warn()', done => {
@@ -42,5 +44,7 @@ describe('index', () => {
       console.warn.restore()
       done()
     })
+      console.warn('finally made it')
   })
+
 })
