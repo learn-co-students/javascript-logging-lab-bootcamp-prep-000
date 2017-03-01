e7cd4ee96858b2d528cd2a698bbe476ee7e40cd8
Lucas Kisabeth <lukey.mac@outlook.com>
2017-03-01 20:41:06 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,3 @@
+console.error("HEEEEELLLPPP!!!")
+console.log("If you're reading this.. things appear to be working fine!")
+console.warn("It's dangerous to go alone!") //Maybe not something you'd expect and ATM to say... but get the reference?
