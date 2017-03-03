3048b4e1d0696b5fadfb1be56eb37fe83839deb9
W L <wlei2@binghamton.edu>
2017-03-03 03:26:24 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,4 @@
+console.error("HALP!")
+console.log("Logging to pass the second test")
+console.warn("Please don't forget to save the index.js before running your learn test/ learn submit.\
+ Otherwise it runs as if the index.js is still empty")
