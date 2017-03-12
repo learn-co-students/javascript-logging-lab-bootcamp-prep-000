49ac8b966d36eb4e6cf8b0c5ca74a2462d285f88
Yassir  Abdosh <mohammed.yassir@outlook.com>
2017-03-12 05:35:38 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,3 @@
+console.error("Now I'm beginning to get the hang of this!")
+console.warn("When the going get's tough, the tough get going")
+console.log("Look, I'm gonna need you to be patient. Remember... clouds and dirt")
