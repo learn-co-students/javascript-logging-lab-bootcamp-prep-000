9141882205831afb4e4593727e7b3d204fdccff7
Denecia Anthony <deneciaanthony694@gmail.com>
2017-03-16 21:08:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-conosole.warn("updating please wait")
-var word
-word= 'bird'
-console.log(word)
-console.log(word)// 'bird'
-//declare the variable
-var word
-word = 'dog'
-console.log(word)// 'dog'
-console.log("I would be a logger.")
+console.error("Halp!")
+console.error("HALP!")
 console.log("I am a logger.")
+console.warn("Update coming")
