486caef87d72e98392e97cfee8075e79e5383a61
Denecia Anthony <deneciaanthony694@gmail.com>
2017-03-16 19:58:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 conosole.warn("updating please wait")
+var word
+word= 'bird'
+console.log(word)
+console.log(word)// 'bird'
+//declare the variable
+var word
+word = 'dog'
+console.log(word)// 'dog'
+console.log("I would be a logger.")
+console.log("I am a logger.")
