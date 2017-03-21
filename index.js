0cce88aada68982d5f393817aebdef513d96cc92
Maureen Ashley <maureenmuteti24@gmail.com>
2017-03-21 08:40:06 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,3 @@
+console.error('Invalid Input! Please Try Again :(');
+console.log('Thank You For Using Our Services. Hope To See You Soon :)');
+console.warn('Amount Is Above Limit, Are You Sure You Want To Proceed?');
