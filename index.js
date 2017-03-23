e9d6b295aa76a46b784c63026c4f2d49da1f2de7
Rebecca Kopec <rebecca.kopec@gmail.com>
2017-03-23 20:34:38 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,3 @@
+console.error ("HALP")
+console.log ("Did I do it right this time?")//always remember to read instructions and FOLLOW them
+console.warn ("warning: you will mess up if you don't follow instructions")
