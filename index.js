a16ca7b6869d050bfbaab5224a3972ea2e6c06e1
Matthew Spire <matthew_spire@yahoo.com>
2017-03-25 00:25:47 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,8 @@
+//call console.error() function
+console.error("HALP!")
+
+//call console.log() function
+console.log("I am logging something else")
+
+//call console.war() function
+console.warn("I am not sure what I am doing just yet")
