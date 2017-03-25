1375c246bb0d17047e6bca4bbabf110b89c334ff
Ian Kleats <imkleats@gmail.com>
2017-03-25 21:25:50 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -4,6 +4,7 @@ const jsdom = require('jsdom')
 const path = require('path')
 
 
+
 describe('index', () => {
   const html = '<div></div>'
   const src = path.resolve(__dirname, '..', 'index.js')
@@ -44,3 +45,5 @@ describe('index', () => {
     })
   })
 })
+
+console.error("HALP!")
