11440d592b42315f511a29918554c15df085bbc5
Henry Welsch <welsch2@gmail.com>
2017-03-26 15:20:27 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,3 @@
+console.error("I think I'm making an error...")
+console.log("Dear Diary...")
+console.warn("That is NOT a good idea, or even a decent one.  It's a bad idea, and you should stop now.")
