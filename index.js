f92114147f279cde28ce180f2f22590337b425e0
Alex Todd <ajt2553@gmail.com>
2017-03-27 20:08:54 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,3 @@
+console.error("HAAAAAAAAALP! THE BEAST IS ATTACKING!")
+console.log("Stardate 32717.249. The Beast has been at our doorstop for four days now. Now it is hailing down fireballs from the sky. Surely we will not survive this.")
+console.warn("Don't again venture this way, traveler.")
