ced6b1dbbcc9c23c4e5c2e7ff6ea1bab8b0970fb
Marcus Chaves <chavesmarcus@gmail.com>
2017-03-30 22:34:59 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -7,7 +7,6 @@ const path = require('path')
 describe('index', () => {
   const html = '<div></div>'
   const src = path.resolve(__dirname, '..', 'index.js')
-
   it('calls console.error()', done => {
     const spy = expect.spyOn(console, 'error').andCallThrough()
 
