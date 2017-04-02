f50e5dfac1e9dd65be1a9f1fa17b45e3901888b8
Syed Muhammad Mehdi Hussain <mehdihussaini138@gmail.com>
2017-04-02 18:56:29 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -19,7 +19,8 @@ describe('index', () => {
       done()
     })
   })
-
+  
+console.error("HALP!")
   it('calls console.log()', done => {
     const spy = expect.spyOn(console, 'log').andCallThrough()
 
