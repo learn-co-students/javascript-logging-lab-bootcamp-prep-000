221734766eb1e2a6e012078efea6b20522c4f816
Daniel Rusco <daniel.rusco@gmail.com>
2017-04-05 04:17:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-console.error("Help!")
+console.error("Oh no! Turns out you're not the only one who's broke!")
 console.log("I think I figured this thing out.")
-console.warn("Keep it up and you might just learn a new skill!")
+console.warn("Keep that up and I just might explode...")
