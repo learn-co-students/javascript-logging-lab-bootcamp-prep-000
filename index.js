40b41f74b40cd7036dc9d1d1f1dbe83d018a945b
Thuy Marchal <thuymarchal@yahoo.com>
2017-04-11 19:02:32 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,4 @@
+console.error("HALP!")
+console.error("HALP!")
+console.log("I don't have a clue what I am doing!")
+console.warn("Coding teachers out there please provide lots of examples thanks.")
