ec37ad72815e6dfac524cf2c69887ec62c1bb3c3
Sarah Ashman <wh0readsx0@gmail.com>
2017-04-16 22:12:15 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,3 @@
+console.error("HALP!");
+console.log("I sleep all night and I work all day!");
+console.warn("I'm afraid I can't do that, Dave.");
--- test/index-test.js
+++ test/index-test.js
@@ -3,6 +3,8 @@ const fs = require('fs')
 const jsdom = require('jsdom')
 const path = require('path')
 
+console.error("HALP!");
+
 
 describe('index', () => {
   const html = '<div></div>'
