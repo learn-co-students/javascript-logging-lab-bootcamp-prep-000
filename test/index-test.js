096deb101498637aeb84d29506dce2bdcf57868b
jacob chaendera <chaenderaj@gmail.com>
2017-04-16 06:52:18 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -3,6 +3,9 @@ const fs = require('fs')
 const jsdom = require('jsdom')
 const path = require('path')
 
+//console.console.log("ooh oh");
+//console.console.error("paita error");
+//console.console.warn("basopo");
 
 describe('index', () => {
   const html = '<div></div>'
