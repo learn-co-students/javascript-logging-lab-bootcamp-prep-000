1dec31cecca123d54eb06e8528c79e54e633cb76
QIAN LI <lqustc@gmail.com>
2017-04-16 22:12:21 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,3 @@
+console.error("This is used to fix the first failure.");
+console.log("This is used to log info rather than errors.");
+console.warn("This is neither info nor error. It is just a warning that something not good just happened but the app is still running fine.");
