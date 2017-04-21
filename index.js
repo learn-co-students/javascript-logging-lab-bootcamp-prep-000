f4751b95dd510d5b6cbc38a26ba3c119ade757a3
Hillah Mansher <hamansher@gmail.com>
2017-04-21 16:01:21 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,6 @@
+console.error("help")
+/* console.error is a function but console.error() is a called upon function*/
+/*spies are little bits of code that keep track of whether or not theuu where called.
+we use them to make sure that a function is called when expected*/
+console.log("logblog")
+console.warn("You are deep in shit")
